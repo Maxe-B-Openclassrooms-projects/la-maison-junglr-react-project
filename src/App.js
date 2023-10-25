a071c8bf442b8aef2f9f92c1038da1ed6cec9edf
MaxeBar <maxence.baranger.mb@gmail.com>
2023-10-25 13:48:07 +0200

    Ajout d'un composant Compteur React
    - gestion d'un package babbel suite à une erreur, déjà présent
    - modfication et mise en commentaire du composant App par défaut
    fourni.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Coucou LoneStone</p>
+        <p>Coucou Petit test React </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,7 +22,22 @@ function App() {
         </a>
       </header>
     </div>
+
+  );
+}
+function Compteur() {
+  const [compte, setCompte] = useState(0);
+
+  return (
+    <div>
+      <h1>Bonjour, ceci est un composant React fonctionnel !</h1>
+      <p>Compte actuel: {compte}</p>
+      <button onClick={() => setCompte(compte + 1)}>Ajouter</button>
+      <button onClick={() => setCompte(compte - 1)}>Soustraire</button>
+    </div>
   );
 }
 
-export default App;
+ReactDOM.render(<Compteur />, document.getElementById('root'));
+
+export default Compteur;
